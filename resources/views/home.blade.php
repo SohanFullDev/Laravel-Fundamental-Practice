039830bdd64008f08b363fd776e8b68567778b51
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-08-23 16:57:35 +0600

    11.2_loops in templates_part2_s7t19

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,20 @@
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ratione quaerat vero a, ullam reiciendis earum distinctio nihil exercitationem quidem neque odit aliquid quasi esse, repudiandae, adipisci non placeat.
 
     <div class="row mt-5">
-       @foreach ($blogs as $blog)
+      @for ( $i= 0;  $i< count($blogs); $i++)
+
+
        <div class="col-md-4">
         <div class="card">
             <div class="card-body">
-                <h2> {{ $blog['title'] }}</h2>
-                <p>{{ $blog['body'] }}</p>
+                <h2> {{ $blogs[$i]['title'] }}</h2>
+                <p>{{ $blogs[$i]['body'] }}</p>
 
             </div>
         </div>
 
     </div>
-       @endforeach
+    @endfor
 
     </div>
 
